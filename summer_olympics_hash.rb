2fb5ec956bb459a57a6fd61e492a771961b0102b
mayawilli <nikeprosandbigbows23@gmail.com>
2018-08-08 17:32:19 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -9,9 +9,14 @@ def create_olympics_hash
   puts summer_olympics_hash
 end
 create_olympics_hash
+
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  create_olympics_hash
+  summer_olympics_hash["Atlanta"]=1996
 end
+
+add_a_key_value_pair
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
